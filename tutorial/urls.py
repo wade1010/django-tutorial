0c38387e39bcd87597619486d2bc455799b472c8
yaya <640297@qq.com>
2023-06-10 20:52:34 +0800

    tutorial 1 serializer

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,17 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework import routers
 from quickstart import views
 
 router = routers.DefaultRouter()
-router.register(r'user',views.UserViewSet)
-router.register(r'group',views.GroupViewSet)
+router.register(r'user', views.UserViewSet)
+router.register(r'group', views.GroupViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls',namespace='rest_framework')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('snippets/', include('snippets.urls')),  # http://localhost:8000/snippets/snippets/    http://localhost:8000/snippets/snippets/1/
 ]
